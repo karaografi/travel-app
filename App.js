a1fc53da28d61e53d9a25e2a525a976d68908e2a
karaografi <karayahya84@gmail.com>
2023-06-02 17:19:14 +0300

    travel-app

---
+++ App.js
@@ -0,0 +1,10 @@
+import TourDetailScreen from "./app/screens/TourDetailScreen";
+import TOURS from "./app/config/TOURS";
+import HomeScreen from "./app/screens/HomeScreen"
+
+export default function App() {
+  return <HomeScreen />;
+}
+
+
+{/* <TourDetailScreen tour={TOURS[3]} /> */}
